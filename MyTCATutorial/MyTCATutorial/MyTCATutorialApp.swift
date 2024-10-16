8a2f69deea9e167f70a9b03a00909471b6ef10f4
cys <cys@iquest.kr>
2024-10-16 14:48:37 +0900

    feat: 프로젝트 생성

---
+++ MyTCATutorial/MyTCATutorial/MyTCATutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTCATutorialApp.swift
+//  MyTCATutorial
+//
+//  Created by YuSeongChoi on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTCATutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
