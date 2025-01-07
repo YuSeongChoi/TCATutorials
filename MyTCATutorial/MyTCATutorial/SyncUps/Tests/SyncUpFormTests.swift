b02e03d76527f863703a3a7f2d79f027802f2c5a
YuSeongChoi <marine8743@naver.com>
2025-01-07 15:01:57 +0900

    tuto: TCA Testing the add sync-up flow

--- MyTCATutorial/MyTCATutorial/SyncUps/Tests/SyncUpFormTests.swift
+++ MyTCATutorial/MyTCATutorial/SyncUps/Tests/SyncUpFormTests.swift
@@ -11,20 +11,60 @@ import ComposableArchitecture
 @testable import MyTCATutorial
 
 final class SyncUpFormTests: XCTestCase {
-    func testAddAttendee() async {
-        let store = TestStore(
-            initialState: SyncUpForm.State(
-                syncUp: SyncUp(id: SyncUp.ID())
-            )
-        ) {
-            SyncUpForm()
+    func addSyncUpNonExhaustive() async {
+        let store = TestStore(initialState: SyncUpsList.State()) {
+            SyncUpsList()
         } withDependencies: {
             $0.uuid = .incrementing
         }
+        store.exhaustivity = .off(showSkippedAssertions: true)
         
-        await store.send(.addAttendeeButtonTapped) {
-            $0.focus = .attendee(Attendee.ID(0))
-            $0.syncUp.attendees.append(Attendee.ID(0))
+        await store.send(.addSyncUpButtonTapped)
+        
+        let editedSyncUp = SyncUp(
+            id: SyncUp.ID(0),
+            attendees: [
+                Attendee(id: Attendee.ID(0), name: "Chodan"),
+                Attendee(id: Attendee.ID(0), name: "Magenta")
+            ],
+            title: "QWER"
+        )
+        await store.send(\.addSyncUp.binding.syncUp, editedSyncUp)
+        
+        await store.send(.confirmAddButtonTapped) {
+          $0.syncUps = [editedSyncUp]
+        }
+    }
+    
+    func testAddAttendee() async {
+        let store = TestStore(initialState: SyncUpsList.State()) {
+          SyncUpsList()
+        } withDependencies: {
+          $0.uuid = .incrementing
+        }
+
+
+        await store.send(.addSyncUpButtonTapped) {
+          $0.addSyncUp = SyncUpForm.State(
+            syncUp: SyncUp(id: SyncUp.ID(0))
+          )
+        }
+        
+        let editedSyncUp = SyncUp(
+            id: SyncUp.ID(0),
+            attendees: [
+                Attendee(id: Attendee.ID(), name: "Chodan"),
+                Attendee(id: Attendee.ID(), name: "Magenta")
+            ],
+            title: "QWER"
+        )
+        await store.send(\.addSyncUp.binding.syncUp, editedSyncUp) {
+          $0.addSyncUp?.syncUp = editedSyncUp
+        }
+
+        await store.send(.confirmAddButtonTapped) {
+          $0.addSyncUp = nil
+          $0.syncUps = [editedSyncUp]
         }
     }
     
--- MyTCATutorial/MyTCATutorial/SyncUps/Tests/SyncUpsListTests.swift
+++ MyTCATutorial/MyTCATutorial/SyncUps/Tests/SyncUpsListTests.swift
@@ -13,6 +13,19 @@ import XCTest
 
 @MainActor
 struct SyncUpsListTests {
+    @Test("추가 테스트")
+    func addSyncUp() async {
+        let store = TestStore(initialState: SyncUpsList.State()) {
+            SyncUpsList()
+        } withDependencies: {
+            $0.uuid = .incrementing
+        }
+        
+        await store.send(.addSyncUpButtonTapped) {
+            $0.addSyncUp = SyncUpForm.State(syncUp: SyncUp(id: SyncUp.ID(0)))
+        }
+    }
+    
     @Test("삭제 테스트")
     func deletion() async throws {
         let store = TestStore(
